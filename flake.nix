ecc3033d21e5eb35a79c454c2e55a948408a1336
Ryan Faulhaber <ryf@sent.as>
2025-10-19 14:27:51 -0400

    initialize project

---
+++ flake.nix
@@ -0,0 +1,68 @@
+{
+  description = "the iota text editor";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    ...
+  }: let
+    projectName = "iota";
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [];
+      flake.overlays.rustOverlay = inputs.rust-overlay.overlays.default;
+      systems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.rustOverlay
+          ];
+        };
+
+        formatter = pkgs.alejandra;
+
+        packages.${projectName} = pkgs.rustPlatform.buildRustPackage {
+          pname = projectName;
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+
+          meta.mainProgram = projectName;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            rust-bin.stable.latest.default
+            clippy
+            rust-analyzer
+            cargo-nextest
+          ];
+        };
+      };
+
+      flake = {};
+    };
+}
